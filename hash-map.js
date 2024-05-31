41a5f352acd5a6ba6f5382c61f9f0e0b8c6315bd
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-05-31 10:03:23 +0800

    Create a HashMap factory that contains methods
    suchs as:
      set, get, has, remove, length, clear, values, and entries

--- hash-map.js
+++ hash-map.js
@@ -1,44 +1,38 @@
-import { linkedLists, Node } from "./linked-lists.js";
+import linkedLists from "./linked-lists.js";
+import Node from "./node.js";
+import Bucket from "./buckets.js";
 
-const hash = ({ key }) => {
-  let hashCode = 0;
-  let ky = key;
+const HashMap = () => {
+  const hash = ({ key }) => {
+    let hashCode = 0;
+    let ky = key;
 
-  const primeNumber = 31;
-  const num = 16;
+    const primeNumber = 31;
+    const num = 16;
 
-  for (let i = 0; i < ky.length; i++) {
-    hashCode = (primeNumber * hashCode + ky.charCodeAt(i)) % num;
-  }
+    for (let i = 0; i < ky.length; i++) {
+      hashCode = (primeNumber * hashCode + ky.charCodeAt(i)) % num;
+    }
 
-  return hashCode;
+    return hashCode;
+  };
+
+  const set = ({ key, value, Node }) => {
+    const ky = key,
+      val = value,
+      linkedListsAsBucket = linkedLists(),
+      node = Node;
+  };
+
+  const get = ({ key }) => {};
+  const has = ({ key }) => {};
+  const remove = ({ key }) => {};
+  const length = () => {};
+  const keys = () => {};
+  const values = () => {};
+  const entries = () => {};
+
+  return { set, get, has, remove, length, keys, values, entries };
 };
 
-let buckets = [];
-
-const set = ({ key, value }) => {
-  const ky = key;
-  const val = value;
-
-  // use bucket to create bucket in an array as a linked lists
-  const bucket = linkedLists();
-  let hashCode = hash({ key: ky });
-
-  if (buckets[hashCode] === undefined) {
-    // dont create a bucket instead use existing bucket add to last lists
-    bucket.append({
-      value: { ky, val },
-      next: null,
-      node: Node,
-    });
-    // asign bucket a new linked lists
-    buckets[hashCode] = bucket;
-  }
-  if (buckets[hashCode] !== undefined) {
-    buckets[hashCode].append({ value: { ky, val }, next: null, node: Node });
-  }
-
-  return { ky, val };
-};
-
-export { set, hash, buckets };
+export default HashMap;
