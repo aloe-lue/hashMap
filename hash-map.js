e22f88e3761d73173325aa02b5d11177dc9050a9
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-06-01 20:30:29 +0800

    Create the following hashMap methods
    such as :
      get(key) that returns the value of that key
      has(key) returns true or false
      remove(key) removes the entry from that bucket
    
    there are more potential bugs on these methods especially the linked
    lists method remove for it was slow and needed to check twice to confirm
    it was removed using hashMap.get(key) method

--- hash-map.js
+++ hash-map.js
@@ -3,6 +3,10 @@ const HashMap = () => {
     let hashCode = 0;
     let ky = key;
 
+    if (ky === undefined) {
+      return null;
+    }
+
     const primeNumber = 31;
     const num = 16;
 
@@ -13,7 +17,7 @@ const HashMap = () => {
     return hashCode;
   };
 
-  const set = ({ key, value, Node, mybuckets, hashFunc, linkedLists }) => {
+  const set = ({ key, value, }, { Node, mybuckets, hashFunc, linkedLists }) => {
     const ky = key,
       val = value,
       linkedListsAsBucket = linkedLists(),
@@ -21,11 +25,13 @@ const HashMap = () => {
       buckets = mybuckets,
       hashF = hashFunc;
 
+    // reuse code when creating a bucket
     const hashCode = hashF({ key: ky });
     const bucket = buckets.getBuckets()[hashCode];
 
     buckets.updateBucketList({ newBucket: bucket, newKey: ky, newValue: val });
 
+    // handle collision
     buckets.addBucketList({
       newBucket: bucket,
       newKey: ky,
@@ -33,6 +39,7 @@ const HashMap = () => {
       Node: node,
     });
 
+    // create bucket
     buckets.createBucket({
       newBucket: bucket,
       newKey: ky,
@@ -46,9 +53,60 @@ const HashMap = () => {
     return buckets.getBuckets();
   };
 
-  const get = ({ key }) => {};
-  const has = ({ key }) => {};
-  const remove = ({ key }) => {};
+  const get = ({ newKey }, { newBuckets, hashFunc, }) => {
+    const key = newKey,
+      buckets = newBuckets,
+      hashF = hashFunc;
+
+    const hashCode = hashF({ key }),
+      bucket = buckets.getBuckets()[hashCode];
+
+    if (bucket === undefined) {
+      return null;
+    };
+
+    const index = bucket.find({ value: key }),
+      value = bucket.at({ index });
+
+    return value === null ? null : value.val;
+  };
+
+  const has = ({ newKey }, { newBuckets, hashFunc }) => {
+    const key = newKey,
+      buckets = newBuckets,
+      hashF = hashFunc;
+
+    const hashCode = hashF({ key }),
+      bucket = buckets.getBuckets()[hashCode];
+
+    if (bucket === undefined) {
+      return false;
+    }
+
+    const index = bucket.find({ value: key });
+    const list = bucket.at({ index });
+
+    return list !== null;
+  };
+
+  const remove = ({ newKey }, { newBuckets, hashFunc }) => {
+    const key = newKey,
+      buckets = newBuckets,
+      hashF = hashFunc;
+
+    const hashCode = hashF({ key }),
+      bucket = buckets.getBuckets()[hashCode];
+
+    if (bucket === undefined) {
+      return false;
+    }
+
+    const index = bucket.find({ value: key }),
+      list = bucket.removeAt({ index });
+
+    return list === null;
+  };
+
   const length = () => {};
   const keys = () => {};
   const values = () => {};
