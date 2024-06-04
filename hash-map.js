40f3142a5188640141090fd82e63d6cf3c5bb497
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-06-04 20:07:11 +0800

    Create 3 hashMap methods such as get has and remove
    hashMap.get(key) receives a key and returns value of the key
    
    hashMap.has(key) returns either true or false
    
    hash.remove(key) well yeah removes key and returns either true or false
    when it's deleted or not

--- hash-map.js
+++ hash-map.js
@@ -28,7 +28,12 @@ const HashMap = () => {
     const hashCode = hash(ky);
     const bucket = buckets[hashCode];
     const bucketAsLinkedLists = linkedLists();
-
+    /**
+     * @info
+     * handle collisions and update values within linked lists
+     *   you want to use bucket.contain because using this method returns
+     *   undefined on using it when bucket is an object type.
+     */
     if (bucket !== undefined && bucket.contain) {
       if (bucket.contain({ value: ky })) {
         bucket.set({ value: ky, assignVal: val });
@@ -43,6 +48,7 @@ const HashMap = () => {
       return bucket;
     }
 
+    // when key generates the same hashCode but difference key
     if (bucket !== undefined && bucket.ky !== ky) {
       bucketAsLinkedLists.append({
         key: bucket.ky,
@@ -55,15 +61,71 @@ const HashMap = () => {
       return bucketAsLinkedLists;
     }
 
-    // handle collission
-    buckets[hashCode] = { ky, val };
-    return buckets;
+    return (buckets[hashCode] = { ky, val });
   };
 
-  const get = (key) => {};
-  const has = (key) => {};
-  const remove = (key) => {};
-  const length = () => {};
+  const get = (key) => {
+    const ky = key;
+
+    const hashCode = hash(ky),
+      bucket = buckets[hashCode];
+
+    if (bucket === null || bucket === undefined) return null;
+
+    if (bucket.contain) {
+      const index = bucket.find({ value: ky }),
+        value = bucket.at({ index });
+
+      return value === null ? null : value.val;
+    }
+
+    return bucket.val;
+  };
+
+  const has = (key) => {
+    const ky = key;
+
+    if (!ky) return false;
+
+    const hashCode = hash(ky),
+      bucket = buckets[hashCode];
+
+    if (bucket === undefined || bucket === null) return false;
+
+    if (bucket.contain) {
+      const index = bucket.find({ value: ky }),
+        value = bucket.at({ index });
+
+      return value !== null;
+    }
+
+    return bucket.ky !== null;
+  };
+
+  const remove = (key) => {
+    const ky = key;
+
+    if (!ky) return null;
+
+    const hashCode = hash(ky),
+      bucket = buckets[hashCode];
+
+    if (bucket === undefined || bucket === null) return null;
+
+    if (bucket.contain) {
+      const index = bucket.find({ value: ky }),
+        value = bucket.removeAt({ index });
+
+      return !bucket.contain({ value: value.key })
+    }
+
+    buckets[hashCode] = undefined;
+    return bucket === undefined;
+  };
+
+  const length = () => {
+
+  };
   const keys = () => {};
   const values = () => {};
   const entries = () => {};
