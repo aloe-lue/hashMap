01605c07a3b114df7d83af47e2b2a7de726b45c9
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-06-08 16:43:01 +0800

    Test the initBucketSize() but here its bucketSize()

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-import buckets from "./buckets.js";
 import HashMap from "./hash-map.js";
 
 const hashMap = HashMap();
@@ -14,35 +13,40 @@ const testDatas = [
   { key: "Jena", value: "My name is Jena, Nice to meet you!" },
   { key: "Selena", value: "My name is Selena, Nice to meet you!" },
 
-  { key: 'alexis', value: 'my name alexis' },
-  { key: 'alexis', value: 'I\'m supersmarto, and kindo' },
-  { key: 'alexis', value: 'im mcdonaldu and i serve peoples foodo' },
-  { key: 'alexis', value: 'im amara, im super great of balarino', },
-  { key: 'pena', value: ' pena desu' },
-  { key: 'pena', value: 'naruhodo' },
-  { key: 'pia', value: 'pia desu' },
+  { key: "alexis", value: "my name alexis" },
+  { key: "alexis", value: "I'm supersmarto, and kindo" },
+  { key: "alexis", value: "im mcdonaldu and i serve peoples foodo" },
+  { key: "alexis", value: "im amara, im super great of balarino" },
+  { key: "pena", value: " pena desu" },
+  { key: "pena", value: "naruhodo" },
+  { key: "pia", value: "pia desu" },
 
-  { key: 'nica', value: 'im nica' },
-  { key: 'manok', value: 'my name is manok' },
-  { key: 'patu', value: ' pena desu' },
-  { key: 'naku', value: ' pena desu' },
-  { key: 'hatdok', value: ' pena desu' },
+  { key: "nica", value: "im nica" },
+  { key: "manok", value: "my name is manok" },
+  { key: "patu", value: " pena desu" },
+  { key: "naku", value: " pena desu" },
+  { key: "hatdok", value: " pena desu" },
 
+  { key: "abida", value: "im abida" },
+  { key: "alid", value: "im alido" },
+  { key: "bersu", value: "im versu" },
+  { key: "nesma", value: "im nesma" },
 ];
-
+hashMap.bucketsSize();
 testDatas.forEach((data) => hashMap.set(data.key, data.value));
+/**
+hashMap.remove("alexis");
+hashMap.get("alexis");
+hashMap.has("alexis");
+
+hashMap.remove("Carlos");
+hashMap.get("Carlos");
+hashMap.has("Carlos");
+*/
 
 console.log(
-  hashMap.remove('alexis'),
-  hashMap.get('alexis'),
-  hashMap.has('alexis'),
-
-  hashMap.remove('Carlos'),
-  hashMap.get('Carlos'),
-  hashMap.has('Carlos'),
-
-  hashMap.remove('Carlos'),
-
-
-  hashMap.get('pena'),
+  hashMap.length(),
+  hashMap.keys(),
+  hashMap.values(),
+  hashMap.entries()
 );
